1cde0860aa376b3b01b1c005c4beb28a16956a57
ianmiell <ian.miell@gmail.com>
2015-09-20 15:49:39 +0100

    cat

--- postgres_9_5/postgres_9_5.py
+++ postgres_9_5/postgres_9_5.py
@@ -7,13 +7,14 @@ from shutit_module import ShutItModule
 class postgres_9_5(ShutItModule):
 
 	def build(self,shutit):
+		# From: docker hub, postgres 9.5
 		shutit.send("""cat > /root/stop_postgres.sh <<< 'service postgresql stop'""")
 		shutit.send('apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8')
 		shutit.send('''echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main' 9.5 > /etc/apt/sources.list.d/pgdg.list''')
 		shutit.send('apt-get update')
 		shutit.install('postgresql-common')
 		shutit.send(r'''sed -ri 's/#(create_main_cluster) .*$/\1 = false/' /etc/postgresql-common/createcluster.conf''')
-		shutit.install('postgresql-9.5=9.5~alpha2-1.pgdg80+1')
+		shutit.install('postgresql-9.5=9.5~alpha2-1.pgdg80+1 postgresql-contrib-9.5=9.5~alpha2-1.pgdg80+1')
 		shutit.add_line_to_file('# postgres', '/root/start_postgres.sh')
 		shutit.add_line_to_file("echo Setting shmmax for postgres", '/root/start_postgres.sh')
 		shutit.add_line_to_file('service postgresql start', '/root/start_postgres.sh', force=True)
