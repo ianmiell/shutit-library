b3ec2b251be5a04e9043a5d68b9ec5bd2f0631e8
Ian Miell <ian.miell@gmail.com>
2016-05-18 06:07:32 +0100

    link

--- vagrant/vagrant.py
+++ vagrant/vagrant.py
@@ -73,12 +73,12 @@ class vagrant(ShutItModule):
 		if not shutit.command_available('vagrant'):
 			if shutit.get_current_shutit_pexpect_session_environment().install_type == 'apt':
 				pw = shutit.get_env_pass('Input your sudo password to install vagrant')
-				shutit.send('wget -qO- https://releases.hashicorp.com/vagrant/vagrant_' + vagrant_version + '_' + processor + '.deb > /tmp/vagrant.deb',note='Downloading vagrant and installing')
+				shutit.send('wget -qO- https://releases.hashicorp.com/vagrant/' + vagrant_version + '/vagrant_' + vagrant_version + '_' + processor + '.deb > /tmp/vagrant.deb',note='Downloading vagrant and installing')
 				shutit.multisend('sudo dpkg -i /tmp/vagrant.deb',{'assword':pw})
 				shutit.send('rm -f /tmp/vagrant.deb')
 			elif shutit.get_current_shutit_pexpect_session_environment().install_type == 'yum':
 				pw = shutit.get_env_pass('Input your sudo password to install vagrant')
-				shutit.send('wget -qO- https://dl.bintray.com/mitchellh/vagrant/vagrant_' + vagrant_version + '_' + processor + '.rpm > /tmp/vagrant.rpm',note='Downloading vagrant and installing')
+				shutit.send('wget -qO- https://releases.hashicorp.com/vagrant/' + vagrant_version + '/vagrant_' + vagrant_version + '_' + processor + '.rpm > /tmp/vagrant.rpm',note='Downloading vagrant and installing')
 				shutit.multisend('sudo rpm -i /tmp/vagrant.rpm',{'assword':pw})
 				shutit.send('rm -f /tmp/vagrant.rpm')
 			else:
