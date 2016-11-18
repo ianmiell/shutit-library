d0bdb9a0dbf51f5b896353203cd98db886c7d007
Ian Miell <ian.miell@gmail.com>
2016-11-18 11:56:09 +0000

    colon

--- vagrant/vagrant.py
+++ vagrant/vagrant.py
@@ -37,17 +37,17 @@ class vagrant(ShutItModule):
 					shutit.install('libvirt-devel')
 					shutit.install('qemu-kvm')
 					pw = shutit.get_env_pass()
-					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -r https://rubygems.org/',{'assword:':pw})
-					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -a http://rubygems.org/', {'Do you want to add this insecure source?':'y','assword:':pw})
-					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem update --system --no-doc',{'assword:':pw})
-					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -r http://rubygems.org/',{'assword:':pw})
-					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -a https://rubygems.org/',{'assword:':pw})
-					shutit.multisend('sudo vagrant plugin install vagrant-libvirt',{'assword:':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -r https://rubygems.org/',{'assword':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -a http://rubygems.org/', {'Do you want to add this insecure source?':'y','assword':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem update --system --no-doc',{'assword':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -r http://rubygems.org/',{'assword':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -a https://rubygems.org/',{'assword':pw})
+					shutit.multisend('sudo vagrant plugin install vagrant-libvirt',{'assword':pw})
 		except:
 			pass
 		if shutit.cfg['shutit-library.virtualization.virtualization.virtualization']['virt_method'] == 'libvirt':
 			pw = shutit.get_env_pass()
-			shutit.multisend('sudo systemctl start libvirtd',{'assword:':pw})
+			shutit.multisend('sudo systemctl start libvirtd',{'assword':pw})
 		else:
 			if shutit.send_and_get_output("""vagrant version  | head -1 | awk '{print $3}'""") < '1.8.6':
 				shutit.log('Vagrant version may be too low!')
