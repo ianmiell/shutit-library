2af609ddca924974f025c706484e90400d831a7f
Ian Miell <ian.miell@gmail.com>
2016-11-18 11:54:46 +0000

    pw

--- vagrant/vagrant.py
+++ vagrant/vagrant.py
@@ -36,16 +36,18 @@ class vagrant(ShutItModule):
 					shutit.install('libvirt')
 					shutit.install('libvirt-devel')
 					shutit.install('qemu-kvm')
-					shutit.send('sudo /opt/vagrant/embedded/bin/gem source -r https://rubygems.org/')
-					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -a http://rubygems.org/', {'Do you want to add this insecure source?':'y'})
-					shutit.send('sudo /opt/vagrant/embedded/bin/gem update --system --no-doc')
-					shutit.send('sudo /opt/vagrant/embedded/bin/gem source -r http://rubygems.org/')
-					shutit.send('sudo /opt/vagrant/embedded/bin/gem source -a https://rubygems.org/')
-					shutit.send('sudo vagrant plugin install vagrant-libvirt')
+					pw = shutit.get_env_pass()
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -r https://rubygems.org/',{'assword:':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -a http://rubygems.org/', {'Do you want to add this insecure source?':'y','assword:':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem update --system --no-doc',{'assword:':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -r http://rubygems.org/',{'assword:':pw})
+					shutit.multisend('sudo /opt/vagrant/embedded/bin/gem source -a https://rubygems.org/',{'assword:':pw})
+					shutit.multisend('sudo vagrant plugin install vagrant-libvirt',{'assword:':pw})
 		except:
 			pass
 		if shutit.cfg['shutit-library.virtualization.virtualization.virtualization']['virt_method'] == 'libvirt':
-			shutit.send('sudo systemctl start libvirtd')
+			pw = shutit.get_env_pass()
+			shutit.multisend('sudo systemctl start libvirtd',{'assword:':pw})
 		else:
 			if shutit.send_and_get_output("""vagrant version  | head -1 | awk '{print $3}'""") < '1.8.6':
 				shutit.log('Vagrant version may be too low!')
