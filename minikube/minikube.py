67e1f4cbf61820704ed4b1ef053d494415f28e2b
Ian Miell <ian.miell@gmail.com>
2017-03-05 09:21:36 +0000

    available

--- minikube/minikube.py
+++ minikube/minikube.py
@@ -4,13 +4,13 @@ class minikube(ShutItModule):
 
 
 	def build(self, shutit):
-		if not shutit.command_exists('minikube'):
+		if not shutit.command_available('minikube'):
 			shutit.send('curl https://storage.googleapis.com/minikube/releases/v0.16.0/minikube-linux-amd64 > minikube')
 			shutit.send('chmod +x minikube')
 			shutit.send('mv minikube /usr/local/bin/minikube')
 		if not shutit_send_and_get_output('''minikube status | grep 'Does Not Exist' | wc -l | awk '{print $1}' ''') == '1':
 			shutit.send('./minikube start')
-		if not shutit.command_exists('kubectl'):
+		if not shutit.command_available('kubectl'):
 			shutit.send('curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl')
 			shutit.send('chmod +x kubectl')
 			shutit.send('mv kubectl /usr/local/bin/kubectl')
