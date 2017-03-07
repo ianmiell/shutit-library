978f5d478ec5f900298de73b4390acb5243091c2
Ian Miell <ian.miell@gmail.com>
2017-03-07 08:33:14 +0000

    mk

--- minikube/minikube.py
+++ minikube/minikube.py
@@ -5,17 +5,17 @@ class minikube(ShutItModule):
 
 	def build(self, shutit):
 		if not shutit.command_available('kubectl'):
-			if shutit.send_and_get_output('uname') == 'Darwin':																																   
-				shutit.send('curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl')																																	 
-			else:																																												 
-				shutit.send('curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl')														   
+			if shutit.send_and_get_output('uname') == 'Darwin':
+				shutit.send('curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl')
+			else:
+				shutit.send('curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl')
 			shutit.send('chmod +x kubectl')
 			shutit.send('sudo mv kubectl /usr/local/bin/kubectl')
 		if not shutit.command_available('minikube'):
-			if shutit.send_and_get_output('uname') == 'Darwin':																																   
-				shutit.send('curl -LO https://storage.googleapis.com/minikube/release/$(curl -s https://storage.googleapis.com/minikube/release/stable.txt)/bin/darwin/amd64/minikube')
-			else:																																												 
-				shutit.send('curl -LO https://storage.googleapis.com/minikube/release/$(curl -s https://storage.googleapis.com/minikube/release/stable.txt)/bin/linux/amd64/minikube')
+			if shutit.send_and_get_output('uname') == 'Darwin':
+				shutit.send('curl -LO https://storage.googleapis.com/minikube/releases/$(curl -s https://storage.googleapis.com/minikube/releases/stable.txt)/minikube-darwin-amd64')
+			else:
+				shutit.send('curl -LO https://storage.googleapis.com/minikube/releases/$(curl -s https://storage.googleapis.com/minikube/releases/stable.txt)/minikube-linux-amd64')
 			shutit.send('chmod +x minikube')
 			shutit.send('sudo mv minikube /usr/local/bin/minikube')
 		if not shutit.send_and_get_output('''minikube status | grep 'Does Not Exist' | wc -l | awk '{print $1}' ''') == '1':
