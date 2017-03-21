c2a4d13c0ae538b1bfb44ce7e7ec38cea1f780d2
Ian Miell <ian.miell@gmail.com>
2017-03-21 08:09:14 +0000

    mk

--- minikube/minikube.py
+++ minikube/minikube.py
@@ -11,7 +11,7 @@ class minikube(ShutItModule):
 				shutit.send('curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl')
 			shutit.send('chmod +x kubectl')
 			shutit.send('sudo mv kubectl /usr/local/bin/kubectl')
-		if not shutit.command_available('minikube'):
+		def install_minikube():
 			if shutit.send_and_get_output('uname') == 'Darwin':
 				shutit.send('curl -LO https://storage.googleapis.com/minikube/releases/v$(curl -s https://storage.googleapis.com/minikube/releases/stable.txt)/minikube-darwin-amd64')
 				shutit.send('mv minikube-darwin-amd64 minikube')
@@ -20,10 +20,11 @@ class minikube(ShutItModule):
 				shutit.send('mv minikube-linux-amd64 minikube')
 			shutit.send('chmod +x minikube')
 			shutit.send('sudo mv minikube /usr/local/bin/minikube')
-		else:
-			shutit.pause_point('Want to remove minikube?')
+		if shutit.command_available('minikube'):
+			shutit.pause_point('Want to remove minikube? If so, continue')
 			shutit.send('rm -rf ~/.minikube')
 			shutit.multisend('minikube delete',{'Y/n':'Y'})
+		install_minikube()
 		if not shutit.send_and_get_output('''minikube status | grep 'Does Not Exist' | wc -l | awk '{print $1}' ''') == '1':
 			shutit.send('minikube start')
 		return True
