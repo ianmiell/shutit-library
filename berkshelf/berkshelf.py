a95dd15bc635d2fb18d38a583041d14c2518e699
Ian Miell <ian.miell@gmail.com>
2017-06-13 08:35:07 +0100

    sendandexpectdead

--- berkshelf/berkshelf.py
+++ berkshelf/berkshelf.py
@@ -5,7 +5,7 @@ class berkshelf(ShutItModule):
 
 	def build(self, shutit):
 		if not shutit.command_available('berks'):
-			if shutit.send_and_expect('whoami') != 'root':
+			if shutit.send('whoami') != 'root':
 				shutit.fail('must be root')
 			if not shutit.command_exists('gem'):
 				shutit.fail('gem must be available')
--- inspec/inspec.py
+++ inspec/inspec.py
@@ -5,13 +5,13 @@ class inspec(ShutItModule):
 
 	def build(self, shutit):
 		if not shutit.command_available('rake'):
-			if shutit.send_and_expect('whoami') != 'root':
+			if shutit.send('whoami') != 'root':
 				shutit.fail('must be root')
 			if not shutit.command_exists('gem'):
 				shutit.fail('gem must be available')
 			shutit.send('gem install rake')
 		if not shutit.command_available('inspec'):
-			if shutit.send_and_expect('whoami') != 'root':
+			if shutit.send('whoami') != 'root':
 				shutit.fail('must be root')
 			if not shutit.command_exists('gem'):
 				shutit.fail('gem must be available')
